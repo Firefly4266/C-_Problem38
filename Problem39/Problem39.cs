02c280225d413333a87d9748bf590b9973458ea5
Firefly4266 <gaines.johnp@gmail.com>
2017-10-15 16:14:41 -0700

    Exercise 39 completed.

--- Problem39/Problem39.cs
+++ Problem39/Problem39.cs
@@ -11,75 +11,104 @@ namespace Problem39
         void Start();
         int Drive();
         void Stop();
+        //void Empty();
+        //bool Running { get; }
     }
 
-    public class Car : ICar
+    class Car : ICar
     {
+        private bool running = false;
         int fuel = 10;
 
         public void Start()
         {
-            Console.WriteLine("\n The car has started.\n");
+            if (fuel > 0)
+            {
+                if (running == false)
+                {
+                    Console.WriteLine("\n The car has started.\n");
+                    running = true;
+                }
+                else
+                {
+                    Console.WriteLine($"\n Car already running, please choose again.\n");
+                }
+            }
+            else
+            {
+                Console.WriteLine("\n You are out of fuel, vehicle will not start.");
+                running = false;
+            }
         }
 
         public int Drive()
         {
-            if(fuel <= 0)
+            if(fuel > 0)
             {
-                Console.WriteLine($"\n You have {fuel} gallons fuel.\n");
-                return fuel;
+                if(running == true)
+                {
+                    fuel = fuel - 1;
+                    Console.WriteLine($"\n Now driving. You have {fuel} gallons of fuel remaining.\n");
+                    return fuel;
+                }
+                else
+                {
+                    Console.WriteLine($"\n You must first start the car before you can drive.\n");
+                    return fuel;
+                }
             }
             else
             {
-                Console.WriteLine($"\n You have {fuel} gallons fuel.\n");
-                return fuel = fuel - 1;
+                Console.WriteLine("\n You are out of fuel, vehicle will not start.");
+                return fuel;
             }
         }
         public void Stop()
         {
-            Console.WriteLine("\n The car has stopped.\n");
+            if(running == true)
+            {
+                Console.WriteLine("\n The car has stopped.\n");
+                running = false;
+            }
+            else
+            {
+                Console.WriteLine("\n Car was never started. \n");
+            }
         }
+
+
     }
     class MyClass39
     {
         public void Problem39()
-        {
-            bool running = false;
-            bool stopped = true;
-            bool empty = false;
-
+        {            
             Car car = new Car();
             
             Console.WriteLine("\n Greetings user, what would you like to do?");
             Console.WriteLine(" (Choices are 'start' 'drive' 'stop')");
             string input = Console.ReadLine().ToLower();
 
-            while (input != null && empty == false && stopped == true)
+            while (input != null )
             {
-                if(input == "start" && stopped == true && running == false)
+                if(input == "start")
                 {
                     car.Start();
-                    running = true; stopped = false;
                 }
-                else if (input == "drive" && running == true)
+                else if (input == "drive")
                 {
-                    car.Drive();
-                    stopped = false;
+                    car.Drive();                   
                 }
-                else if (input == "stop" && running == true && stopped == false)
+                else if (input == "stop")
                 {
                     car.Stop();
-                    stopped = true;
                 }
                 else
                 {
-                    //Console.WriteLine(" (Choices are 'start' 'drive' 'stop')\n");
-                    //input = Console.ReadLine( ).ToLower();
-                Console.WriteLine($"\n Car already running, please choose again.\n");
-                Console.WriteLine(" (Choices are 'start' 'drive' 'stop')");
-                input = Console.ReadLine().ToLower();
+                    Console.WriteLine("\n Please enter a vaild command.\n");
                 }
-            }
+                Console.WriteLine("\n What would you like to do? (Choices are 'start' 'drive' 'stop')");
+                input = Console.ReadLine().ToLower();
+            }            
         }
     }
 }
